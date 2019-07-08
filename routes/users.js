ef02a68d666a60fde2abf10ee46ec30d44471542
lokesh-kadiyala98 <33837730+lokesh-kadiyala98@users.noreply.github.com>
2019-07-08 17:46:48 +0530

    Initial commit

---
+++ routes/users.js
@@ -0,0 +1,53 @@
+const express = require('express')
+const app = express()
+const morgan = require('morgan')
+const mysql = require('mysql')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+const PORT = process.env.PORT || 5000;
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'Jyothi123',
+    database: 'pola_db'
+})
+
+app.use(cors())
+
+app.use(bodyParser.urlencoded({extended: false}))
+
+app.use(express.static('./public'))
+
+app.use(morgan('combined'))
+
+app.get("/heart_count", (req, res) => {
+  const querString = 'SELECT COUNT(DISTINCT(email)) AS heartCount FROM opinion WHERE liked'
+  connection.query(querString, (err, rows, fields) => {
+    res.json( rows )
+  })
+})
+
+app.get('/send_opinion', (req, res) => {
+  
+  const query = 'INSERT INTO opinion(customerName, number, email, experience, liked) VALUES(?, ?, ?, ?, ?)'
+
+  connection.query(query, [req.query.name, req.query.number, req.query.email, req.query.experience, req.query.like === 'true' ], (err, rows, fields) => {
+    console.log(err);
+    if(err){
+      res.json({ success: false })
+      res.sendStatus(500)
+      return
+    }
+
+    res.json({ success: true })
+  })  
+
+})
+
+app.listen(PORT, () => {
+    console.log("Server is up and listening on ", PORT)
+})
+
+module.exports = app;
