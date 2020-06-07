791dcef4e66d36c31826ddde40a6b3324d1d2b98
lokesh-kadiyala98 <lokesh.pandu1998@gmail.com>
2020-06-07 16:42:19 +0530

    simplified api code

--- app.js
+++ app.js
@@ -1,41 +1,49 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const app = express()
+const mongodb = require('mongodb')
+const MongoClient = mongodb.MongoClient;
+require('dotenv').config()
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+app.use(cors())
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
 
-var app = express();
+const PORT = process.env.PORT || 5000
+const DBurl = process.env.DBurl
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.get('/', (req, res) => {
+    res.send({ message: "APIEndpoint up and running" }).status(200)
+})
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.get('/heart_count', (req, res) => {
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+  MongoClient.connect(DBurl, async (err, client) => {
+    if(err)
+      res.send({ error: 'Database Connection: Seems like something went wrong!!' })
+    else {
+      const db = client.db('polas')
+      const count = await db.collection('opinions').countDocuments({ like: true })
+      client.close()
+      res.send({ count: count })
+    }
+  })
+})
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+app.post('/send_opinion', (req, res) => {
+  MongoClient.connect(DBurl, (err, client) => {
+    if(err)
+      res.send({ error: 'Database Connection: Seems like something went wrong!!' })
+    else {
+      const db = client.db('polas')
+      db.collection('opinions').insertOne(req.body)
+      client.close()
+      res.send({ success: 'Thankyou!! We took a note of that.' })
+    }
+  })
+})
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
+app.listen(PORT, () => {
+  console.log('ON PORT ', PORT)
+})
